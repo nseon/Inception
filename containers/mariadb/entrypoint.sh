d37a0306e39fc1f47effa7d34e0133d9d501425f
nseon <nseon@nseon.42.fr>
2026-03-05 11:33:14 -0600

    good work

--- containers/mariadb/entrypoint.sh
+++ containers/mariadb/entrypoint.sh
@@ -1,5 +1,19 @@
 #!/bin/sh
 
-mariadb-install-db --datadir=/var/lib/mysql
+if [ ! -d "/var/lib/mysql/mysql" ]; then
+	db_pwd=$(cat /run/secrets/db_password)
+	mariadb-install-db --datadir=/var/lib/mysql
+	
+	cat << EOF > /tmp/init.sql
+FLUSH PRIVILEGES;
+CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;
+CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${db_pwd}';
+GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`${MYSQL_USER}\`@'%';
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${db_pwd}';
+FLUSH PRIVILEGES;
+EOF
 
-exec mariadbd
+	mariadbd --datadir=/var/lib/mysql --bootstrap < /tmp/init.sql
+fi
+
+exec mariadbd --datadir=/var/lib/mysql
