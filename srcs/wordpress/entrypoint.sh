9258d6101709b5ae4b359a7e6ac9d15c0d9824e2
nseon <nseon@nseon.42.fr>
2026-03-25 08:35:45 -0500

    fix: domain name & version

--- srcs/wordpress/entrypoint.sh
+++ srcs/wordpress/entrypoint.sh
@@ -3,7 +3,7 @@
 cd /usr/share/webapps/wordpress
 
 if [ ! -f "wp-config.php" ]; then
-    DB_PWD=$(cat /run/secrets/db_password)
+    db_pwd=$(cat /run/secrets/db_password)
     admin_user=$(sed '1!d' /run/secrets/wp_admin)
     admin_passwd=$(sed '2!d' /run/secrets/wp_admin)
     admin_mail=$(sed '3!d' /run/secrets/wp_admin)
@@ -15,13 +15,13 @@ if [ ! -f "wp-config.php" ]; then
     
     sed -i "s|database_name_here|$MYSQL_DATABASE|g" wp-config.php
     sed -i "s|username_here|$MYSQL_USER|g" wp-config.php
-    sed -i "s|password_here|$DB_PWD|g" wp-config.php
+    sed -i "s|password_here|$db_pwd|g" wp-config.php
     sed -i "s|localhost|mariadb|g" wp-config.php
 fi
 
 if ! wp core is-installed; then
     wp core install \
-    --url=nseon.42.fr \
+    --url=$DOMAIN_NAME \
     --title="Inception" \
     --admin_user=$admin_user \
     --admin_password=$admin_passwd \
